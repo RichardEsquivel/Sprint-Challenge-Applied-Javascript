4892b0c6c8cc792cb98546b769b33768f2106589
RichardEsquivel <rlesquivel@gmail.com>
2019-08-09 11:43:44 -0500

    working on getting header to appear properly

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+	//Creating Header div and adding class of .header
+	const div1 = document.createElement('div');
+	div1.classList.add('header');
+
+	//Creating dateHere for date contained in span
+	const dateHere = document.createElement('span');
+	dateHere.classList.add('date');
+	dateHere.textContent = 'SMARCH 28, 2019';
+	div1.append(dateHere);
+
+	// h1 to hold Lambda Times Text
+	const titleH1 = document.createElement('h1');
+	titleH1.textContent = 'Lambda Times';
+	div1.append(titleH1);
+
+	//Span to hold temperature
+	const temp = document.createElement('span');
+	temp.textContent = '98°';
+	div1.append('temp');
+
+	return div1;
+}
+
+//Appending Header function to the .header-container
+document.querySelector('.header-container').append(Header());
