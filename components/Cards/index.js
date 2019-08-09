d072069675f5cc86c2d52f9f8e1a0ef9600468d8
RichardEsquivel <rlesquivel@gmail.com>
2019-08-09 12:56:33 -0500

    Created tabs and had them populate data, creating eventListener to populate tabs with needed topics

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//creating axios.get in order to create a component for each article in the list
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then((response) => {
+	console.log(response);
+	Object.values(response.data.articles).forEach((responses) => {
+		responses.forEach((responses) => {
+			const buildsCards = document.querySelector('.cards-container');
+			buildsCards.append(cardCreate(responses));
+		});
+	});
+});
+
+//Creating function for building DOM element of the cards, using axios Data source responses to build
+//out content and src of element content
+
+function cardCreate(responses) {
+	const cards = document.createElement('div');
+	const headlines = document.createElement('div');
+	const authors = document.createElement('div');
+	const imgContainer = document.createElement('div');
+	const newImgs = document.createElement('img');
+	const authorNames = document.createElement('span');
+
+	//adding class lists for styling of elements
+	cards.classList.add('card');
+	headlines.classList.add('headline');
+	authors.classList.add('author');
+	imgContainer.classList.add('img-container');
+
+	headlines.textContent = responses.headline;
+	newImgs.src = responses.authorPhoto;
+	authorNames.textContent = responses.authorName;
+
+	//Appending elements to each other to build out the div class of card
+	imgContainer.append(newImgs);
+	authors.append(imgContainer);
+	authors.append(authorNames);
+	headlines.append(authors);
+	cards.append(headlines);
+
+	return cards;
+}
